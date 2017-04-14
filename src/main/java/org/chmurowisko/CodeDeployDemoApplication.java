dc32a3d78d899be59f5bbd56ef1253b0f40abc33
jbogacz <Rower123>
2017-04-14 15:04:09 +0200

    First commit

---
+++ src/main/java/org/chmurowisko/CodeDeployDemoApplication.java
@@ -0,0 +1,12 @@
+package org.chmurowisko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDeployDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeDeployDemoApplication.class, args);
+	}
+}
