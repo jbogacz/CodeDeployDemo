dc8bf1b05f86f473086f7dad15edc9596154356b
jbogacz <Rower123>
2017-04-14 15:40:27 +0200

    Travis deploy configuration

---
+++ src/main/java/org/chmurowisko/HelloController.java
@@ -0,0 +1,17 @@
+package org.chmurowisko;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jbogacz on 14.04.2017.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping(path = "hello")
+    public String index() {
+        return "Greetings from Chmurowisko!";
+    }
+
+}
